09f6464890d1fc558a4b5e3e1345970d1a6c6ea6
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-06 00:04:45 +0800

    Add sysai-register package __init__.py with version

---
+++ src/sysai-register/__init__.py
@@ -0,0 +1,10 @@
+"""
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+File: sysai-register/__init__.py
+Desc: AI Model Register - Independent package for model registration
+Date: 2025-10-22
+Author: Liu Mingran
+"""
+
+__version__ = '1.0.0'
+
