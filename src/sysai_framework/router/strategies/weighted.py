d38640fa3ffdf7cf32abb24a452d741effbaf269
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-07 21:31:08 +0800

    Add weighted.py header imports and logger

---
+++ src/sysai_framework/router/strategies/weighted.py
@@ -0,0 +1,17 @@
+"""
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+File: strategies/weighted.py
+Desc: Weighted random load balance strategy
+Date: 2025-01-26
+Author: Liu Mingran
+"""
+
+import random
+from typing import List, Optional
+
+from sysai_framework.config.model_config import ModelConfig
+from sysai_framework.router.routing_strategy import BaseRoutingStrategy
+
+import logging
+
+logger = logging.getLogger(__name__)
