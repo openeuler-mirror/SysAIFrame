b0f3121040f7c81760024053d3deb4bcc1b33a29
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-07 08:33:44 +0800

    Add model_config.py header

---
+++ src/sysai_framework/config/model_config.py
@@ -0,0 +1,8 @@
+"""
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+File: model_config.py
+Desc: Model configuration management for SysAIFrame
+     Handles model configuration loading, validation and management
+Date: 2025-10-28
+Author: Liu Mingran
+"""
