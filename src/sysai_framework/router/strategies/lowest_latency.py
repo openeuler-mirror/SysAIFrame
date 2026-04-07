29909cb11d3fd072068ca95118cc30f8d83bc0bf
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-07 21:34:21 +0800

    Add __init__ method to LowestLatencyStrategy

--- src/sysai_framework/router/strategies/lowest_latency.py
+++ src/sysai_framework/router/strategies/lowest_latency.py
@@ -20,4 +20,16 @@ logger = logging.getLogger(__name__)
 
 class LowestLatencyStrategy(BaseRoutingStrategy):
     """Lowest latency load balance strategy - selects model with lowest average latency"""
-    pass
+
+    def __init__(self, config_manager=None):
+        super().__init__(config_manager)
+        self._latency_history: Dict[str, deque] = {}
+        self._lock = threading.Lock()
+
+        if config_manager:
+            runtime_config = config_manager.routing_config.runtime
+            self._latency_window = runtime_config.load_balance.options.latency_window
+            self._latency_buffer = runtime_config.load_balance.options.latency_buffer
+        else:
+            self._latency_window = 10
+            self._latency_buffer = 0.1
