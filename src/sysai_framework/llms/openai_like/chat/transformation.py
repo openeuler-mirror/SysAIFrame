7bc4b08ff92abc5b2a86bac2a9625f99c5b9c7be
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-07 21:15:23 +0800

    Add OpenAILikeChatConfig class skeleton

--- src/sysai_framework/llms/openai_like/chat/transformation.py
+++ src/sysai_framework/llms/openai_like/chat/transformation.py
@@ -11,3 +11,13 @@ Author: Liu Mingran
 from typing import Optional, Tuple
 
 from ...openai.chat.gpt_transformation import OpenAIGPTConfig
+
+
+class OpenAILikeChatConfig(OpenAIGPTConfig):
+    """
+    Configuration for Chat Completion API compatible endpoints.
+
+    This class is used for any provider that follows the Chat Completion API specification
+    but is not one of the specifically supported providers.
+    """
+    pass
