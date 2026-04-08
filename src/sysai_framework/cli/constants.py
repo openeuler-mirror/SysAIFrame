7e14b28af23d63fb251d14dc9413b92ec132c26a
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-08 11:24:45 +0800

    Add CLI constants

---
+++ src/sysai_framework/cli/constants.py
@@ -0,0 +1,9 @@
+"""
+Constants for CLI tools.
+
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+"""
+import os
+
+# Default configuration path
+DEFAULT_CONFIG_PATH = os.environ.get('SYSAIFRAME_CONFIG_PATH', '/etc/sysaiframe/models.yaml')
