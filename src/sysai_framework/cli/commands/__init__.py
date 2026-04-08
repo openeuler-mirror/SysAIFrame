ef81de53d7162c889f546ad93d70b127ba36fada
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-08 11:24:21 +0800

    Add commands package init

---
+++ src/sysai_framework/cli/commands/__init__.py
@@ -0,0 +1,13 @@
+"""
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+File: cli/commands/__init__.py
+Desc: CLI commands package
+Date: 2025-10-22
+Author: Liu Mingran
+"""
+
+from .model import model
+from .routing import routing
+from .service import service
+
+__all__ = ['model', 'routing', 'service']
