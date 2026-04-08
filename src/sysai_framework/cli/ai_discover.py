ae85ea304bf0fd64140433b643c55b47ba727f1d
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-08 11:18:44 +0800

    Add ai_discover.py header and cli group

---
+++ src/sysai_framework/cli/ai_discover.py
@@ -0,0 +1,17 @@
+"""
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+File: cli/ai_discover.py
+Desc: Service discovery CLI tool
+Date: 2025-10-22
+Author: Liu Mingran
+"""
+
+import click
+import sys
+import time
+
+
+@click.group()
+def cli():
+    """SysAIFrame service discovery tool"""
+    pass
