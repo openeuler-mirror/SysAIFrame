6e27cdad43ef907e1c8ecaac236314f3a1db5dc9
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-08 19:24:07 +0800

    Add validators package init

---
+++ src/sysai_framework/cli/validators/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+File: cli/validators/__init__.py
+Desc: CLI validators package
+Date: 2025-10-22
+Author: Liu Mingran
+"""
+
+from .model_validator import ModelValidator
+
+__all__ = ['ModelValidator']
