72e17e492c2828aed16aa2486d5b36aaf0ec8409
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-08 11:22:58 +0800

    Add watch command to ai_discover

--- src/sysai_framework/cli/ai_discover.py
+++ src/sysai_framework/cli/ai_discover.py
@@ -28,3 +28,28 @@ def list(service_type, timeout):
 
     # TODO: Implement actual discovery using DiscoveryManager
     click.echo("No services found (discovery not yet implemented)")
+
+
+@cli.command()
+@click.argument('service_name')
+def info(service_name):
+    """Show detailed information about a service"""
+    click.echo(f"Service: {service_name}")
+    # TODO: Implement service info retrieval
+    click.echo("Service info not available (discovery not yet implemented)")
+
+
+@cli.command()
+@click.option('--watch', is_flag=True, help='Watch for service changes')
+def watch(watch):
+    """Watch for service changes"""
+    if watch:
+        click.echo("Watching for service changes... (Press Ctrl+C to stop)")
+        try:
+            while True:
+                time.sleep(1)
+                # TODO: Implement service watching
+        except KeyboardInterrupt:
+            click.echo("\nStopped watching")
+    else:
+        click.echo("Use --watch flag to enable watching")
