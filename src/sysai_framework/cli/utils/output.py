dc5a4ab3d311a22053223aaec8fa2da9f400040f
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-08 11:25:17 +0800

    Add output.py header and imports

---
+++ src/sysai_framework/cli/utils/output.py
@@ -0,0 +1,13 @@
+"""
+Copyright (C) 2025 CTyunOS. All Rights Reserved.
+File: cli/utils/output.py
+Desc: CLI output formatting utilities
+Date: 2025-11-27
+Author: Liu Mingran
+"""
+
+import click
+import json
+from typing import Any, Dict, List, Optional
+
+
