020bb798dc8cbca2f58b93d85e9599faf594ce4a
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-09 09:55:56 +0800

    Add remaining imports to main.py

--- src/sysai_framework/main.py
+++ src/sysai_framework/main.py
@@ -8,3 +8,11 @@ Author: Liu Mingran
 """
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from sysai_framework.api.v1 import chat
+from sysai_framework.api.v1 import health
+from sysai_framework.config import config
+import uvicorn
+import logging
+import os
+import atexit
