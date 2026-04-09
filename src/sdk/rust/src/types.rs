167a049c8facda15b1951188410868b9fb412482
Mingran Liu <liumr1@chinatelecom.cn>
2026-04-09 09:41:06 +0800

    Add types.rs header and Message struct definition

---
+++ src/sdk/rust/src/types.rs
@@ -0,0 +1,17 @@
+/*
+ * SysAI Rust SDK - Data types
+ *
+ * Copyright (C) 2025 CTyunOS. All Rights Reserved.
+ */
+
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use zvariant::{OwnedValue, Value};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Message {
+    pub role: String,
+    pub content: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+}
